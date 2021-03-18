3fb553bcdb37fe3dff29dc7266897dd0af9789af
Vlad <vladhopilo@gmail.com>
2021-03-18 15:56:56 +0200

    up

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -6,20 +6,24 @@ import {
 
 class MovieDetailsPage extends Component {
   state = {
-    image: null,
+    image: { title: "dfsf" },
   };
 
   componentDidMount() {
-    const { moveId } = this.props.match.params.movieId;
-    const tezt = { moveId };
-    fetchMoviesDetailsApi(tezt).then((response) => console.log(response));
-    // fetchMoviesDetailsApi(test).then((response) =>
-    //   this.setState({ image: response.data })
-    // );
+    const moveiId = this.props.match.params;
+    // console.log(moveiId);
+    // console.log(this.props.match.params);
+    // const tezt = { moveiId };
+    // fetchMoviesDetailsApi(moveiId).then((response) => console.log(response));
+    fetchMoviesDetailsApi(moveiId).then((response) =>
+      this.setState({ image: response.data })
+    );
   }
   render() {
-    console.log(this.props.match.params.movieId);
-    console.log(this.state.image);
+    // console.log(this.props.match.params.movieId);
+    const image = this.state.image;
+    console.log(image.title);
+    // console.log(this.state.image.map((el) => el.title));
     return <h1>Фильмы</h1>;
   }
 }
