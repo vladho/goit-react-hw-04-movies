61cad2d581be0f9dbeda29521cae5de758627dc0
vladho <vladhopilo@gmail.com>
2021-03-22 10:23:25 +0200

    up

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,47 +1,42 @@
-import React, { Component } from "react";
-import MovieList from "../Component/MovieList/MovieList";
-import { fetchMoviesSearchApi } from "../services/movies-api";
+import React, { Component } from "react"
+import MovieList from "../Component/MovieList/MovieList"
+import { fetchMoviesSearchApi } from "../services/movies-api"
 
 class Movies extends Component {
   state = {
     query: "",
     // movies: null,
-  };
+  }
 
   handleChange = (e) => {
-    this.setState({ query: e.currentTarget.value });
-  };
+    this.setState({ query: e.currentTarget.value })
+  }
 
   handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     // this.setState(query)
     // console.log(this.state.query);
-    fetchMoviesSearchApi(this.state.query).then((responce) =>
-      this.setState({ movies: [...responce.data.results] })
-    );
+    fetchMoviesSearchApi(this.state.query).then((responce) => this.setState({ movies: [...responce.data.results] }))
     // console.log(this.state);
-  };
+  }
 
   componentDidMount() {
-    fetchMoviesSearchApi(this.state.query).then((responce) =>
-      this.setState({ movies: [...responce.data.results] })
-    );
+    fetchMoviesSearchApi(this.state.query).then((responce) => this.setState({ movies: [...responce.data.results] }))
   }
 
   render() {
+    console.log(this.props.location)
     return (
       <>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.query} />
           <button type="submit"> Search</button>
         </form>
-        {this.state.movies !== undefined && (
-          <MovieList movies={this.state.movies} search={this.state.query} />
-        )}
+        {this.state.movies !== undefined && <MovieList movies={this.state.movies} search={this.state.query} />}
         {/* {<MovieList movies={this.state.movies} />} */}
       </>
-    );
+    )
   }
 }
 
-export default Movies;
+export default Movies
