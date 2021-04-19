32983ba20c76d8fbcf1d20119f002a99db74d352
Vlad <vladhopilo@gmail.com>
2021-04-19 09:10:20 +0300

    upd

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -2,24 +2,56 @@ import axios from "axios"
 
 const apiKey = "262170cbb6ca02ba32c782e4d358e8a0"
 
-const fetchMoviesTrendingApi = () => {
-  return axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`)
+const fetchMoviesTrendingApi = async() => {
+  try {const responce =  await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`)
+  return responce;}
+  catch (error) {
+    throw new Error(error)
+  }
 }
 
-const fetchMoviesDetailsApi = ({ movieId }) => {
-  return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
+const fetchMoviesDetailsApi = async ({ movieId }) => {
+ try {
+    const response= await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
+
+  
+  return response;
+}catch (error) {
+  throw new Error(error)
+}
 }
 
-const fetchMoviesCreditApi = ({ movieId }) => {
-  return axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}&language=en-US`)
+
+const fetchMoviesCreditApi = async({ movieId }) => {
+    try {
+    const response= await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}&language=en-US`)
+  
+  return response;
+}catch (error) {
+  throw new Error(error)}
 }
 
-const fetchMoviesReviewsApi = ({ movieId }) => {
-  return axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${apiKey}&language=en-US`)
+
+
+const fetchMoviesReviewsApi = async({ movieId }) => {
+    try {
+    const response= await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${apiKey}&language=en-US`)
+  
+  return response;
+}catch (error) {
+  throw new Error(error)}
 }
 
-const fetchMoviesSearchApi = (query = "cat") => {
-  return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${query}`)
+
+
+const fetchMoviesSearchApi = async(query = "cat") => {
+    try {
+    const response= await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${query}`)
+  return response;
+}catch (error) {
+  throw new Error(error)}
 }
+
+
 
 export { fetchMoviesSearchApi, fetchMoviesReviewsApi, fetchMoviesCreditApi, fetchMoviesDetailsApi, fetchMoviesTrendingApi }
